5e131542085236297010540afb7997f429fc9f43
drivadosayan <techsupport1@drivado.com>
2023-08-05 17:56:22 +0530

    chat ui update

--- frontend/src/component/Sidebar.jsx
+++ frontend/src/component/Sidebar.jsx
@@ -1,18 +1,45 @@
+import {
+  faBell,
+  faEnvelope,
+  faRightFromBracket,
+  faTasks,
+  faUser,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
 import Logo from "../assets/chatlogo.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Sidebar = () => {
   return (
-    <div className="border border-white">
+    <div className=" space-y-3">
       {/* logo */}
-      <div>
+      <div className="">
         <img src={Logo} />
       </div>
       {/* all other button */}
-      <div className="border border-white">all other button</div>
+      <div className="h-[78vh] py-44">
+        <div className="space-y-4">
+          <SidebarComponent icon={faEnvelope} value={"Messages"} />
+          <SidebarComponent icon={faUsers} value={"Groups"} />
+          <SidebarComponent icon={faTasks} value={"Contacts"} />
+          <SidebarComponent icon={faBell} value={"Notifications"} />
+          <SidebarComponent icon={faUser} value={"Profile"} />
+        </div>
+      </div>
       {/* logout */}
-      <div className="border border-white">logout</div>
+      <div className="">
+        <SidebarComponent icon={faRightFromBracket} value={"Logout"}/>
+      </div>
     </div>
   );
 };
 
 export default Sidebar;
+
+export const SidebarComponent = ({ icon, value }) => {
+  return (
+    <div className="flex items-center space-x-4 px-4 py-2 cursor-pointer">
+      <FontAwesomeIcon icon={icon} className="text-white text-3xl" />
+      <h1 className="text-white text-2xl">{value}</h1>
+    </div>
+  );
+};
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -5,6 +5,7 @@ import {
   faAdd,
   faBars,
   faLocationArrow,
+  faSearch,
 } from "@fortawesome/free-solid-svg-icons";
 import { faFaceSmile } from "@fortawesome/free-regular-svg-icons";
 
@@ -16,21 +17,42 @@ const ChatPage = () => {
         <Sidebar />
       </div>
       {/* contact and chat area */}
-      <div className="bg-slate-200 flex space-x-4 w-[85%] rounded-3xl my-4 mr-3 ">
-        {/* contact */}
-        <div className="bg-white w-1/5 rounded-3xl  mt-4 mb-5 ml-3 px-2 py-2">
+      <div className="bg-slate-200 flex space-x-4 w-[85%] rounded-3xl my-4 mr-3 p-4">
+        {/* contact search and profile */}
+        <div className=" w-1/5 rounded-3xl px-2 py-2 bg-white">
           <div className="flex space-x-2 items-center bg-red-200 rounded-full">
             <img src={avatar} alt="" className=" w-12 h-12 rounded-full" />
             <h1>Ron</h1>
           </div>
-          <div>
-            
+          <div className="flex items-center rounded-full space-x-2 relative my-2">
+            <input
+              placeholder="Search Name"
+              className="rounded-full w-full  bg-blue-200 py-3 px-4 outline-none"
+            />
+            <FontAwesomeIcon icon={faSearch} className="absolute right-7" />
+          </div>
+          <div>Recent Chats</div>
+          {/* contat area */}
+          <div className="h-[85%] overflow-y-auto space-y-1 pb-4">
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent isActive={true} />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
+            <EachChatComponent />
           </div>
         </div>
         {/* chat */}
         <div className=" w-4/5 rounded-3xl space-y-3">
           {/* header profile */}
-          <div className="flex justify-between bg-white  rounded-3xl py-4 px-4 h-[10%] mt-3 mr-4">
+          <div className="flex justify-between bg-white  rounded-3xl py-4 px-4">
             <div className="flex space-x-2 items-center">
               <div>
                 <img src={avatar} alt="" className=" w-12 h-12 rounded-full" />
@@ -45,24 +67,22 @@ const ChatPage = () => {
             </div>
           </div>
           {/*chat messages area */}
-          <div className="bg-white rounded-3xl py-4 px-4 h-[70%] mr-4">
-            hello
-          </div>
+          <div className="bg-white rounded-3xl py-4 px-4 h-[71vh]">hello</div>
           {/* send messgae area*/}
-          <div className="flex justify-between items-center bg-white rounded-3xl px-4 h-[12%] mb-2 mr-4">
-            <div className="flex space-x-4 w-[85%]">
+          <div className="flex justify-between items-center bg-white rounded-3xl px-4 py-5">
+            <div className="flex space-x-4 w-[90%]">
               <div className="flex items-center px-4 bg-blue-100 rounded-full">
                 <FontAwesomeIcon icon={faAdd} className="text-xl" />
               </div>
               <div className=" rounded-full w-full">
                 <input
                   placeholder="type message here"
-                  className="w-full rounded-full px-5 py-3 outline-none bg-blue-50"
+                  className="w-full rounded-full px-5 py-3 outline-none bg-blue-50 text-xl"
                 />
               </div>
             </div>
             <div className="flex space-x-4 ">
-              <div className=" px-3 py-2 ">
+              <div className="px-3 py-2 ">
                 <FontAwesomeIcon icon={faFaceSmile} className="text-3xl" />
               </div>
               <div className="px-3 flex items-center bg-blue-100 rounded-full">
@@ -80,3 +100,23 @@ const ChatPage = () => {
 };
 
 export default ChatPage;
+
+export const EachChatComponent = ({ isActive }) => {
+  return (
+    <div
+      className={`flex space-x-3 items-center px-2 py-2 ${
+        isActive
+          ? "bg-blue-500 rounded-full text-white"
+          : "hover:bg-blue-100 hover:rounded-full"
+      }`}
+    >
+      <div>
+        <img src={avatar} alt="" className=" w-12 h-12 rounded-full" />
+      </div>
+      <div>
+        <h1>Harry Potter</h1>
+        <h1 className="text-sm"> last message</h1>
+      </div>
+    </div>
+  );
+};
